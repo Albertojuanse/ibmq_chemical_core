2be974f74caa5881ec1df4be4032bee48618cbfc
Sebastián Lombraña, A <aj.sebastian@alumnos.upm.es>
2018-11-26 18:08:08 +0100

    Load backends

--- GestorDeTareas.py
+++ GestorDeTareas.py
@@ -1,7 +1,10 @@
-"""Esta clase implementa un gestor de tareas con el fin de soptimizar las llamadas al gestor trasero"""
-
 # Conexiones por arquitectura
 import GestorTrasero
 
 class GestorDeTareas():
+    """Esta clase implementa un gestor de tareas con el fin de soptimizar las llamadas al gestor trasero"""
 
+    def __init__(self):
+        #Determina los servidores disponibles
+        self.servidoresReales = GestorTrasero.getservidoresreales()
+        self.servidoresSimuladores = GestorTrasero.getservidoressimuladores()
--- GestorTrasero.py
+++ GestorTrasero.py
@@ -1,1 +1,27 @@
 """Esta clase crea un gestor de las conexiones con las máquinas cuánticas de IBM y los simuladores de Quiskit"""
+
+# importa módulos
+from qiskit import IBMQ, Aer
+
+# Variables globales
+__servidoresreales = []
+__servidoressimuladores = []
+
+def __cargarcredenciales():
+    """Esta función establece conexión con los servidores de IBMQ"""
+    try:
+        IBMQ.load_accounts()
+        return True
+    except:
+        return False
+
+def cargarservidores():
+    """Esta función carga los servidores disponibles"""
+    __cargarcredenciales()
+
+    __servidoresreales = IBMQ.backends()
+    __servidoressimuladores = Aer.backends()
+
+
+def getservidoresreales():
+    """Esta función devuelve un listado con los servidores reales disponibles en linea"""
